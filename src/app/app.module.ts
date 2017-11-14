06e715dc779d68cb265a7445a6404db1490fdecd
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-11-14 17:22:38 -0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { PeopleComponent } from './people/people.component';
+import { FilmsComponent } from './films/films.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { SpeciesComponent } from './species/species.component';
+import { PlanetsComponent } from './planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent,
+    PeopleComponent,
+    FilmsComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    SpeciesComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule
